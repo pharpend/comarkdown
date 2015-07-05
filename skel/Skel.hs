a03837d70939100f99c48c839dfe0cfadd6dee77
Peter Harpending <peter@harpending.org>
2015-07-05 17:05:38 -0600

    80 column lines

--- skel/Skel.hs
+++ skel/Skel.hs
@@ -1,15 +1,15 @@
--- This program is free software: you can redistribute it and/or modify
--- it under the terms of the GNU General Public License as published by
--- the Free Software Foundation, either version 3 of the License, or (at
--- your option) any later version.
+-- This program is free software: you can redistribute it and/or modify it under
+-- the terms of the GNU General Public License as published by the Free Software
+-- Foundation, either version 3 of the License, or (at your option) any later
+-- version.
 -- 
--- This program is distributed in the hope that it will be useful, but
--- WITHOUT ANY WARRANTY; without even the implied warranty of
--- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
--- General Public License for more details.
+-- This program is distributed in the hope that it will be useful, but WITHOUT
+-- ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+-- FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+-- details.
 -- 
--- You should have received a copy of the GNU General Public License
--- along with this program.  If not, see <http://www.gnu.org/licenses/>.
+-- You should have received a copy of the GNU General Public License along with
+-- this program.  If not, see <http://www.gnu.org/licenses/>.
 
 -- | 
 -- Module      : Text.Comarkdown.Skel
@@ -20,8 +20,8 @@
 -- Stability   : experimental
 -- Portability : portable
 -- 
--- Here is a longer description of this module, containing some
--- commentary with @some markup@.
+-- Here is a longer description of this module, containing some commentary with
+-- @some markup@.
 
 module Text.Comarkdown.Skel where
 
