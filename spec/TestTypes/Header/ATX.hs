0c9b630872dd2affaef78da597c6d9b6d765e6d2
Peter Harpending <peter@harpending.org>
2015-07-07 06:32:45 -0600

    Restore old types for ATX header tests

--- spec/TestTypes/Header/ATX.hs
+++ spec/TestTypes/Header/ATX.hs
@@ -16,144 +16,26 @@
 -- |Helper types for ATX-style headers
 module TestTypes.Header.ATX where
 
-import TestTypes.Numeric
-import TestTypes.Text
-
 import Data.Text.Lazy (Text)
 import qualified Data.Text.Lazy as T
 import Test.QuickCheck
 import TestTypes.Text ()
 
 -- |'Text's without line breaks, or starting with '#'s
-newtype ATXHeaderText = ATXHeaderText {unATXHeaderText :: Text}
+newtype ATX = ATX {unATX :: Text}
   deriving (Eq, Show)
 
-instance Arbitrary ATXHeaderText where
+instance Arbitrary ATX where
   arbitrary =
-    fmap ATXHeaderText $
+    fmap ATX $
     suchThat arbitrary $
     \x ->
       not (T.isInfixOf "\n" x || T.isPrefixOf "#" (T.strip x))
 
--- |A non-empty 'ATXHeaderText'.
-newtype ATXHeaderTextNonEmpty = ATXHeaderTextNE {unATXHeaderTextNE :: Text}
+-- |A non-empty 'ATX'.
+newtype ATXNonEmpty = ATXNE {unATXNE :: Text}
   deriving (Eq, Show)
 
-instance Arbitrary ATXHeaderTextNonEmpty where
+instance Arbitrary ATXNonEmpty where
   arbitrary =
-    fmap (ATXHeaderTextNE . unATXHeaderText) $
-    suchThat arbitrary $ 
-    not . T.null . unATXHeaderText
-
--- |An ATX 'Header1'
-newtype ATX1 = ATX1 {unATX1 :: Text}
-
-instance Arbitrary ATX1 where
-  arbitrary = fmap (ATX1 . T.intercalate "\n") 
-                   (listOf singleLine)
-    where singleLine = 
-            do HSpace s <- arbitrary
-               ATXHeaderText t <- arbitrary
-               HSpace u <- arbitrary
-               Peano i <- arbitrary
-               HSpace v <- arbitrary
-               return (mconcat [T.pack $ replicate 1  '#'
-                               ,s
-                               ,t
-                               ,u
-                               ,T.pack (replicate i '#')
-                               ,v])
-
--- |An ATX 'Header2'
-newtype ATX2 = ATX2 {unATX2 :: Text}
-
-instance Arbitrary ATX2 where
-  arbitrary = fmap (ATX2 . T.intercalate "\n") 
-                   (listOf singleLine)
-    where singleLine = 
-            do HSpace s <- arbitrary
-               ATXHeaderText t <- arbitrary
-               HSpace u <- arbitrary
-               Peano i <- arbitrary
-               HSpace v <- arbitrary
-               return (mconcat [T.pack $ replicate 2  '#'
-                               ,s
-                               ,t
-                               ,u
-                               ,T.pack (replicate i '#')
-                               ,v])
--- |An ATX 'Header3'
-newtype ATX3 = ATX3 {unATX3 :: Text}
-
-instance Arbitrary ATX3 where
-  arbitrary = fmap (ATX3 . T.intercalate "\n") 
-                   (listOf singleLine)
-    where singleLine = 
-            do HSpace s <- arbitrary
-               ATXHeaderText t <- arbitrary
-               HSpace u <- arbitrary
-               Peano i <- arbitrary
-               HSpace v <- arbitrary
-               return (mconcat [T.pack $ replicate 3  '#'
-                               ,s
-                               ,t
-                               ,u
-                               ,T.pack (replicate i '#')
-                               ,v])
-
--- |An ATX 'Header4'
-newtype ATX4 = ATX4 {unATX4 :: Text}
-
-instance Arbitrary ATX4 where
-  arbitrary = fmap (ATX4 . T.intercalate "\n") 
-                   (listOf singleLine)
-    where singleLine = 
-            do HSpace s <- arbitrary
-               ATXHeaderText t <- arbitrary
-               HSpace u <- arbitrary
-               Peano i <- arbitrary
-               HSpace v <- arbitrary
-               return (mconcat [T.pack $ replicate 4  '#'
-                               ,s
-                               ,t
-                               ,u
-                               ,T.pack (replicate i '#')
-                               ,v])
-
--- |An ATX 'Header5'
-newtype ATX5 = ATX5 {unATX5 :: Text}
-
-instance Arbitrary ATX5 where
-  arbitrary = fmap (ATX5 . T.intercalate "\n") 
-                   (listOf singleLine)
-    where singleLine = 
-            do HSpace s <- arbitrary
-               ATXHeaderText t <- arbitrary
-               HSpace u <- arbitrary
-               Peano i <- arbitrary
-               HSpace v <- arbitrary
-               return (mconcat [T.pack $ replicate 5  '#'
-                               ,s
-                               ,t
-                               ,u
-                               ,T.pack (replicate i '#')
-                               ,v])
-
--- |An ATX 'Header6'
-newtype ATX6 = ATX6 {unATX6 :: Text}
-
-instance Arbitrary ATX6 where
-  arbitrary = fmap (ATX6 . T.intercalate "\n") 
-                   (listOf singleLine)
-    where singleLine = 
-            do HSpace s <- arbitrary
-               ATXHeaderText t <- arbitrary
-               HSpace u <- arbitrary
-               Peano i <- arbitrary
-               HSpace v <- arbitrary
-               return (mconcat [T.pack $ replicate 6  '#'
-                               ,s
-                               ,t
-                               ,u
-                               ,T.pack (replicate i '#')
-                               ,v])
+    fmap (ATXNE . unATX) $ suchThat arbitrary $ not . T.null . unATX
