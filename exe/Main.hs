2c7b5fb2bf4d22392e99ab733431f12ff397baa6
Peter Harpending <peter@harpending.org>
2015-07-09 21:29:07 -0600

    Use Stackage LTS instead of Stackage nightly.

--- exe/Main.hs
+++ exe/Main.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE CPP #-}
+
 -- This program is free software: you can redistribute it and/or modify it under
 -- the terms of the GNU General Public License as published by the Free Software
 -- Foundation, either version 3 of the License, or (at your option) any later
@@ -22,6 +24,10 @@
 
 module Main where
 
+#if __GLASGOW_HASKELL < 710
+import Control.Applicative
+import Data.Monoid
+#endif
 import Data.Version (showVersion)
 import Paths_comarkdown (version)
 import Options.Applicative
