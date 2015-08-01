648234c882bf0d4949c01677e9fd53bc4e8ef341
Peter Harpending <peter@harpending.org>
2015-07-31 20:46:20 -0600

    Add IO examples

---
+++ tests/IOExamples.hs
@@ -0,0 +1,53 @@
+-- This program is free software: you can redistribute it and/or modify it under
+-- the terms of the GNU General Public License as published by the Free Software
+-- Foundation, either version 3 of the License, or (at your option) any later
+-- version.
+-- 
+-- This program is distributed in the hope that it will be useful, but WITHOUT
+-- ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+-- FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+-- details.
+-- 
+-- You should have received a copy of the GNU General Public License along with
+-- this program.  If not, see <http://www.gnu.org/licenses/>.
+
+-- | 
+-- Module      : IOExamples
+-- Description : Runs the examples.
+-- Copyright   : Copyright 2015 Peter Harpending
+-- License     : GPL-3
+-- Maintainer  : peter@harpending.org
+-- Stability   : experimental
+-- Portability : portable
+
+module IOExamples where
+
+import Control.Monad (forM_)
+import Data.Algorithm.Diff
+import Data.Algorithm.DiffOutput
+import Data.List (sort, isSuffixOf)
+import System.Directory
+import Test.Hspec
+import Text.Comarkdown
+
+spec :: Spec
+spec =
+  parallel $
+  describe "Examples with an input & output file" $
+  do dirPath <- runIO $ makeAbsolute "tests/io-examples/"
+     dirContents <- runIO $ getDirectoryContents dirPath
+     let dirContents' = drop 2 (sort dirContents)
+         inputPaths = filter (isSuffixOf "in.md") dirContents'
+         outputPaths = filter (isSuffixOf "out.md") dirContents'
+     forM_ (zip inputPaths outputPaths) $
+       \(ip,op) ->
+         specify (mconcat ["Compiling ",ip," matches ",op]) $
+         do inputFile <- makeAbsolute (mappend dirPath ip)
+            outputFile <- makeAbsolute (mappend dirPath op)
+            compiledInput <- runComd inputFile
+            output <- readFile outputFile
+            compiledInput `shouldBe` output
+  where runComd :: FilePath -> IO String
+        runComd fp =
+          do pandoc <- runDocument $ parseFile fp
+             return (writePlain def pandoc)
