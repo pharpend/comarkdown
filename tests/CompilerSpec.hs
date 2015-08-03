9edd121006401764005cd6eb3b3f007069d48768
Peter Harpending <peter@harpending.org>
2015-08-03 15:48:57 -0600

    Add some tests for compiler

--- tests/CompilerSpec.hs
+++ tests/CompilerSpec.hs
@@ -22,7 +22,39 @@
 
 module CompilerSpec where
 
+import Control.Monad (forM_)
+import Text.Comarkdown
 import Test.Hspec
+import Test.QuickCheck
 
 spec :: Spec
-spec = return ()
+spec =
+  parallel $
+  describe "AST->Pandoc compiler" $
+  do describe "Outputting plain-text" $
+       do specify "given many Ignore blks, behave like text all in 1 ignore blk" $
+            property $
+            \(xs :: [String]) ->
+              do result1 <- runDocument (mapM_ ignore xs)
+                 result2 <- runDocument (ignore (mconcat xs))
+                 shouldBe (writePlain def result1)
+                          (writePlain def result2)
+          specify "given many Comment blks, output is mempty" $
+            property $
+            \(xs :: [String]) ->
+              do result <- runDocument (mapM_ comment xs)
+                 shouldBe result mempty
+          specify "given many Comment blks interspersed in ignore blocks, same result as ignore blks standalone" $
+            property $
+            \(xs :: [String],ys :: [String]) ->
+              do shuffledList <-
+                   generate (shuffle (mappend (fmap Ignore xs)
+                                              (fmap Comment ys)))
+                 result1 <- runDocument (mapM_ insertPart shuffledList)
+                 result2 <-
+                   runDocument
+                     (forM_ shuffledList $
+                      \case
+                        Ignore s -> ignore s
+                        _ -> return ())
+                 shouldBe result1 result2
